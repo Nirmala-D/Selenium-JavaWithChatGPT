c3ccb9ead0aada1fcd9be20611d337ed014a83e5
Nirmala-D <nirmaladeekonda2015@gmail.com>
2026-03-14 16:30:06 -0600

    Implemented TestListener, screenshot utility, and BaseTest
    context fix for failed test screenshot capture

---
+++ src/main/java/utils/ScreenshotUtil.java
@@ -0,0 +1,29 @@
+package utils;
+
+import org.openqa.selenium.*;
+import java.io.File;
+import org.apache.commons.io.FileUtils;
+
+public class ScreenshotUtil {
+	
+	 public static String captureScreenshot(WebDriver driver, String name) {
+
+	        String path = System.getProperty("user.dir")
+	                + "/screenshots/" + name + ".png";
+
+	        try {
+
+	            File src = ((TakesScreenshot) driver)
+	                    .getScreenshotAs(OutputType.FILE);
+
+	            File dest = new File(path);
+
+	            FileUtils.copyFile(src, dest);
+
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+
+	        return path;
+	    }
+}
